0af947c397d8b250cdca53a6bacc42b54b6850c1
Miguel Espina <50675302+miguelangel43@users.noreply.github.com>
2024-03-24 16:50:22 +0100

    make sure every model is pickled

--- src/classification.py
+++ src/classification.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 import os
 import pickle
 import pandas as pd
+from sklearn.linear_model import SGDClassifier
 
 
 def train_xgb(X_train, y_train):
@@ -44,6 +45,14 @@ def train_svc(X_train, y_train):
     return gs_svc
 
 
+def train_sgd(X_train, y_train):
+    # Initialize SGD classifier
+    model = SGDClassifier(random_state=42)
+    model.fit(X_train, y_train)
+
+    return model
+
+
 def apply_classifiers_original_features(X_train, y_train, X_test, y_test, dataset_name, classifiers=['XGBOOST'], save_flag=True):
     """
     Run classifiers on original data.
@@ -56,7 +65,7 @@ def apply_classifiers_original_features(X_train, y_train, X_test, y_test, datase
         classifiers (list, optional): List of classifiers to apply. Defaults to ['XGBOOST'].
 
     Returns:
-        float: ROC AUC score.
+        float: Accuracy score.
     """
 
     if 'XGBOOST' in classifiers:
@@ -69,7 +78,7 @@ def apply_classifiers_original_features(X_train, y_train, X_test, y_test, datase
                     __file__))) + f'/models/classifiers/on_orig_features/{dataset_name}/xgboost.pkl', 'wb'))
         # Predict using trained classifier
         y_pred = clf.predict(X_test)
-        # Calculate ROC AUC score
+        # Calculate Accuracy score
         score = accuracy_score(y_test, y_pred)
 
     if 'SVC' in classifiers:  # Train SVC classifier
@@ -82,7 +91,20 @@ def apply_classifiers_original_features(X_train, y_train, X_test, y_test, datase
         # Predict using trained classifier
         y_pred = clf.predict(X_test)
 
-        # Calculate ROC AUC score
+        # Calculate Accuracy score
+        score = accuracy_score(y_test, y_pred)
+
+    if 'SGD' in classifiers:  # Train SGD classifier
+        clf = train_sgd(X_train, y_train)
+        # Save trained classifier to a file
+        if save_flag:
+            pickle.dump(
+                clf, open(os.path.dirname(os.path.dirname(os.path.abspath(
+                    __file__))) + f'/models/classifiers/on_orig_features/{dataset_name}/sgd.pkl', 'wb'))
+        # Predict using trained classifier
+        y_pred = clf.predict(X_test)
+
+        # Calculate Accuracy score
         score = accuracy_score(y_test, y_pred)
 
     return score
@@ -137,6 +159,21 @@ def apply_classifiers_reduced_data(reduced_X, y_train, y_test, dataset_name, cla
             # Store model name, score, and best parameters
             scores[('SVC', *key_dim)] = ['SVC', score, clf.best_params_]
 
+    if 'SGD' in classifiers:
+
+        # Iterate over reduced dimensions
+        for key_dim in tqdm(reduced_X, desc='SGD'):
+            clf = train_sgd(reduced_X[key_dim][0].T, y_train)
+            # Save trained classifier to a file
+            if save_flag:
+                pickle.dump(
+                    clf, open(os.path.dirname(os.path.dirname(os.path.abspath(
+                        __file__))) + f'/models/classifiers/on_reduced_data/{dataset_name}/sgd.pkl', 'wb'))
+            y_pred = clf.predict(reduced_X[key_dim][1].T)
+            score = accuracy_score(y_test, y_pred)
+            # Store model name, score, and best parameters
+            scores[('SGD', *key_dim)] = ['SGD', score, clf.get_params()]
+
     # Format scores in a Pandas df
     scores_df = pd.DataFrame.from_dict(scores, orient='index', columns=[
         'Model', 'Score', 'Params']).reset_index()
@@ -144,6 +181,11 @@ def apply_classifiers_reduced_data(reduced_X, y_train, y_test, dataset_name, cla
         scores_df['index'].tolist(), index=scores_df.index)
     scores_df = scores_df.drop('index', axis=1)
     scores_df = scores_df.sort_values('Score', ascending=False)
+
+    # Save scores as CSV
+    scores_df.to_csv(os.path.dirname(os.path.dirname(
+        os.path.abspath(__file__)) + f'/results/scores/{dataset_name}_scores.csv'))
+    print(f'Scores saved at: /results/scores/{dataset_name}_scores.csv')
 
     # Filter for the best configuration
     best_config = scores_df.fillna('').loc[scores_df.sort_values(
@@ -155,7 +197,8 @@ def apply_classifiers_reduced_data(reduced_X, y_train, y_test, dataset_name, cla
     # Save as pickle
     save_path = os.path.dirname(os.path.dirname(os.path.abspath(
         __file__))) + f'/data/{dataset_name}/reduced/reduced_X_best.pkl'
-
     pickle.dump(reduced_X_best, open(save_path, 'wb'))
+    print(
+        f'Reduced data saved at: /data/{dataset_name}/reduced/reduced_X_best.pkl')
 
     return scores_df, reduced_X_best
--- src/dimensionality_reduction.py
+++ src/dimensionality_reduction.py
@@ -11,7 +11,7 @@ import pandas as pd
 import pickle
 
 
-def apply_slmvp(X_train, X_test, y_train, n, type_kernel, gammas=None, poly_order=None, multilabel=None):
+def apply_slmvp(X_train, X_test, y_train, n, type_kernel, dataset_name, gammas=None, poly_order=None, multilabel=None):
     # Get the principal components
     BAux = SLMVPTrain(X=X_train.T, Y=y_train,
                       rank=n,
@@ -20,7 +20,11 @@ def apply_slmvp(X_train, X_test, y_train, n, type_kernel, gammas=None, poly_orde
                       gammaY=gammas,
                       polyValue=poly_order,
                       multilabel=multilabel)
-
+    # Pickle model
+    save_path = os.path.dirname(os.path.dirname(os.path.abspath(
+        __file__))) + f'/models/dimensionality_reduction/{dataset_name}/{str(n)}_dimensions/slmvp_{str(gammas)}.pkl'
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    pickle.dump(BAux, open(save_path, 'wb'))
     # Get the data projected onto the new dimensions
     data_train, data_test = SLMVP_transform(
         BAux.T, X_train.T), SLMVP_transform(BAux.T, X_test.T)
@@ -28,44 +32,66 @@ def apply_slmvp(X_train, X_test, y_train, n, type_kernel, gammas=None, poly_orde
     return data_train, data_test, BAux
 
 
-def apply_lpp(X_train, X_test, n):
+def apply_lpp(X_train, X_test, n, dataset_name):
     lpp = LPP(n_components=n)
     lpp.fit(X_train)
+    # Pickle model
+    save_path = os.path.dirname(os.path.dirname(os.path.abspath(
+        __file__))) + f'/models/dimensionality_reduction/{dataset_name}/{str(n)}_dimensions/lpp.pkl'
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    pickle.dump(lpp, open(save_path, 'wb'))
     X_lpp_train = lpp.transform(X_train)
     X_lpp_test = lpp.transform(X_test)
 
     return X_lpp_train.T, X_lpp_test.T, lpp.projection_
 
 
-def apply_pca(X_train, X_test, n):
+def apply_pca(X_train, X_test, n, dataset_name):
     pca_model = PCA(n_components=n).fit(X_train)
+    # Pickle model
+    save_path = os.path.dirname(os.path.dirname(os.path.abspath(
+        __file__))) + f'/models/dimensionality_reduction/{dataset_name}/{str(n)}_dimensions/pca.pkl'
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    pickle.dump(pca_model, open(save_path, 'wb'))
     X_pca_train = pca_model.transform(X_train)
     X_pca_test = pca_model.transform(X_test)
     # return train, test, eigenvectors, eigenvalues
     return X_pca_train.T, X_pca_test.T, pca_model.components_, pca_model.explained_variance_
 
 
-def apply_lle(X_train, X_test, n, k, _reg):
+def apply_lle(X_train, X_test, n, k, _reg, dataset_name):
     lle = LLE(n_neighbors=k, n_components=n, reg=_reg)
     X_lle_train = lle.fit_transform(X_train)
+    save_path = os.path.dirname(os.path.dirname(os.path.abspath(
+        __file__))) + f'/models/dimensionality_reduction/{dataset_name}/{str(n)}_dimensions/lle.pkl'
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    pickle.dump(lle, open(save_path, 'wb'))
     X_lle_test = lle.transform(X_test)
 
     return X_lle_train.T, X_lle_test.T
 
 
-def apply_kpca(X_train, X_test, n, type_kernel, gamma=None):
+def apply_kpca(X_train, X_test, n, type_kernel, dataset_name, gamma=None):
     kernel_pca = KernelPCA(
         n_components=n, kernel=type_kernel, fit_inverse_transform=True, gamma=gamma
     )
     X_kpca_train = kernel_pca.fit(X_train).transform(X_train)
+    save_path = os.path.dirname(os.path.dirname(os.path.abspath(
+        __file__))) + f'/models/dimensionality_reduction/{dataset_name}/{str(n)}_dimensions/kpca.pkl'
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    pickle.dump(kernel_pca, open(save_path, 'wb'))
     X_kpca_test = kernel_pca.transform(X_test)
 
     return X_kpca_train.T, X_kpca_test.T
 
 
-def apply_lol(X_train, X_test, y_train, n_components):
+def apply_lol(X_train, X_test, y_train, n_components, dataset_name):
     lmao = LOL(n_components=n_components, svd_solver='full')
     lmao.fit(X_train, y_train)
+    save_path = os.path.dirname(os.path.dirname(os.path.abspath(
+        __file__))) + f'/models/dimensionality_reduction/{dataset_name}/{str(n_components-1)}_dimensions/lol.pkl'
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    pickle.dump(lmao, open(save_path, 'wb'))
     X_lol_train = lmao.transform(X_train)
     X_lol_test = lmao.transform(X_test)
 
@@ -104,36 +130,38 @@ def apply_all_dimensionality_reduction(X_train, X_test, y_train, dataset_name,
             key = (str(n_components) + 'Dim', 'SLMVP', 'Radial-Gammas=0.01')
             pbar.set_description(str(key))
             reduced_data[key] = apply_slmvp(
-                X_train, X_test, y_train, n_components, 'radial', gammas=0.01, multilabel=multilabel)
+                X_train, X_test, y_train, n_components, 'radial', dataset_name=dataset_name, gammas=0.01, multilabel=multilabel)
 
             # Execution with gammas=0.1
             key = (str(n_components) + 'Dim', 'SLMVP', 'Radial-Gammas=0.1')
             pbar.set_description(str(key))
             reduced_data[key] = apply_slmvp(X_train, X_test, y_train,
-                                            n_components, 'radial', gammas=0.1, multilabel=multilabel)
+                                            n_components, 'radial', dataset_name=dataset_name, gammas=0.1, multilabel=multilabel)
 
         if 'PCA' in models_list:
             key = (str(n_components) + 'Dim', 'PCA', '')
             pbar.set_description(str(key))
-            reduced_data[key] = apply_pca(X_train, X_test, n_components)
+            reduced_data[key] = apply_pca(
+                X_train, X_test, n_components, dataset_name=dataset_name)
 
         if 'KPCA' in models_list:
             key = (str(n_components) + 'Dim', 'KPCA', 'Radial')
             pbar.set_description(str(key))
             reduced_data[key] = apply_kpca(
-                X_train, X_test, n_components, 'rbf')
+                X_train, X_test, n_components, 'rbf', dataset_name=dataset_name)
 
         if 'LOL' in models_list:
             if multilabel is None:
                 key = (str(n_components) + 'Dim', 'LOL', '')
                 pbar.set_description(str(key))
                 reduced_data[key] = apply_lol(
-                    X_train, X_test, y_train, n_components=n_components+1)
+                    X_train, X_test, y_train, dataset_name=dataset_name, n_components=n_components+1)
 
         if 'LPP' in models_list:
             key = (str(n_components) + 'Dim', 'LPP', '')
             pbar.set_description(str(key))
-            reduced_data[key] = apply_lpp(X_train, X_test, n_components)
+            reduced_data[key] = apply_lpp(
+                X_train, X_test, n_components, dataset_name=dataset_name)
 
         if 'LLE' in models_list:
             k = floor(sqrt(len(X_train)))
@@ -142,7 +170,7 @@ def apply_all_dimensionality_reduction(X_train, X_test, y_train, dataset_name,
                    str(k) + '-reg=' + str(reg))
             pbar.set_description(str(key))
             reduced_data[key] = apply_lle(
-                X_train, X_test, n_components, k, reg)
+                X_train, X_test, n_components, k, reg, dataset_name=dataset_name)
 
     # Save as pickle
     save_path = os.path.dirname(os.path.dirname(os.path.abspath(
